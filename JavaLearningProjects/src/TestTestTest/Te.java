507354045ea3110dbd07d9353cf663bac3dcbfa0
OleksandrStepaniuk <alexstepalina@gmail.com>
2016-12-15 17:47:54 +0200

    1

---
+++ JavaLearningProjects/src/TestTestTest/Te.java
@@ -0,0 +1,15 @@
+package TestTestTest;
+
+//import java.util.Arrays;
+
+//public class Te {
+//
+//	public static void main(String[] args) {
+//		
+//
+//	}
+
+//}
+//for (int i = 0; i < 10; i++){
+//	System.out.println(i);
+//}
