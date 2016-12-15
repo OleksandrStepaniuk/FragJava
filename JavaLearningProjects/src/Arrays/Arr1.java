c34af1a92f8a4a603f1963b2ebc2fef26013e6a6
OleksandrStepaniuk <alexstepalina@gmail.com>
2016-12-15 15:06:39 +0200

    Arrays )

---
+++ JavaLearningProjects/src/Arrays/Arr1.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class Arr1 {
+
+	public static void main(String[] args) {
+		// Вивод содержимого массива
+		int[] arr = {3, 4, 5, 6};
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
