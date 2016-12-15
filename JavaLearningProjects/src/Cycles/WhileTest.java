0431f2ad0c6204331a12a442deb0d4f018d8ee09
OleksandrStepaniuk <alexstepalina@gmail.com>
2016-12-15 15:16:16 +0200

    While

---
+++ JavaLearningProjects/src/Cycles/WhileTest.java
@@ -0,0 +1,15 @@
+package Cycles;
+
+public class WhileTest {
+
+	public static void main(String[] args) {
+		int[] arr = {4, 5};
+		
+		int i = 0;
+		while (i++ < arr.length) {
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
