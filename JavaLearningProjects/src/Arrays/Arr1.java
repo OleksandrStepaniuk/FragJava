cc6c649bae4226e4ee10bba83133b7c9ef0ddfdb
OleksandrStepaniuk <alexstepalina@gmail.com>
2016-12-15 15:07:57 +0200

    new commit

--- JavaLearningProjects/src/Arrays/Arr1.java
+++ JavaLearningProjects/src/Arrays/Arr1.java
@@ -3,7 +3,7 @@ package Arrays;
 public class Arr1 {
 
 	public static void main(String[] args) {
-		// Вивод содержимого массива
+		// sout digits arrays
 		int[] arr = {3, 4, 5, 6};
 		for (int i = 0; i < arr.length; i++) {
 			System.out.println(arr[i]);
